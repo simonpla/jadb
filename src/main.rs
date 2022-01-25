b72680bbd148ed2d6997409b8743b73569358840
simonpla <63154196+simonpla@users.noreply.github.com>
2022-01-25 20:31:53 +0100

    implement read function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-mod modify;
+mod jadb;
 
 fn main() {
     let rlylongstr = "|o\nlabore et dolore magna aliquyam erat\n|o\nStet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
     let path: &str = "../../../test/123";
-    let my_table = modify::Table {
+    let my_table = jadb::Table {
         path: path,
     };
-    println!(" {} {} {} {}",
+    println!(" {} {:?} {} {} {}",
     // modify::Table::create(path),
-    modify::Table::write(rlylongstr, my_table, 1),
-    modify::Row::length(modify::LenType::Fields),
-    modify::Row::length(modify::LenType::Characters),
-    modify::Field::length()
+    jadb::Table::write(rlylongstr, my_table, 1),
+    jadb::Table::read(my_table, 1),
+    jadb::Row::length(jadb::LenType::Fields),
+    jadb::Row::length(jadb::LenType::Characters),
+    jadb::Field::length()
     );
 }
